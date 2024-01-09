05532028edee949c494eaed10d9918f7c545f033
Roman Schlagowsky <roman.schlagowsky@corsair.com>
2024-01-09 11:27:02 +0100

    Fix crash when color is set to SDPedal

--- Sources/StreamDeckKit/Extensions/UIImage+Color.swift
+++ Sources/StreamDeckKit/Extensions/UIImage+Color.swift
@@ -10,6 +10,7 @@ import UIKit
 public extension UIImage {
 
     static func colored(_ color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
+        guard size.width > 0, size.height > 0 else { return nil }
         let rect = CGRect(origin: .zero, size: size)
         UIGraphicsBeginImageContextWithOptions(rect.size, true, 1)
         color.setFill()

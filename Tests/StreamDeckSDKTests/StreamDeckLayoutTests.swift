67b687fb4174ed5f0cc1eb69bb207bf8f92bc822
Alexander Jentz <catnops@gmail.com>
2024-02-05 11:33:45 +0100

    Make test precondition checks throwing
    Co-authored-by: Alexander Jentz <alexander.jentz@corsair.com>

--- Tests/StreamDeckSDKTests/StreamDeckLayoutTests.swift
+++ Tests/StreamDeckSDKTests/StreamDeckLayoutTests.swift
@@ -23,17 +23,17 @@ final class StreamDeckLayoutTests: XCTestCase {
     // MARK: Initial rendering
 
     func test_render_initial_frame() async throws {
-        await robot.use(.regular, rendering: TestViews.SimpleLayout())
+        try await robot.use(.regular, rendering: TestViews.SimpleLayout())
         await robot.assertSnapshot(\.fullscreens[0], as: .image)
     }
 
     // MARK: Key handling
 
     func test_key_down_up() async throws {
-        await robot.use(.regular, rendering: TestViews.SimpleLayout())
+        try await robot.use(.regular, rendering: TestViews.SimpleLayout())
 
-        await robot.keyPress(1, pressed: true)
-        await robot.keyPress(1, pressed: false)
+        try await robot.keyPress(1, pressed: true)
+        try await robot.keyPress(1, pressed: false)
 
         robot.assertEqual(\.fullscreens.count, 1)
         robot.assertEqual(\.keys.count, 2)
@@ -47,13 +47,13 @@ final class StreamDeckLayoutTests: XCTestCase {
     // MARK: Dial handling
 
     func test_dial_rotate_and_click_on_dial_view() async throws {
-        await robot.use(.plus, rendering: TestViews.SimpleLayout())
+        try await robot.use(.plus, rendering: TestViews.SimpleLayout())
 
-        await robot.rotate(2, steps: 3)
-        await robot.rotate(2, steps: -3)
+        try await robot.rotate(2, steps: 3)
+        try await robot.rotate(2, steps: -3)
 
-        await robot.rotaryEncoderPress(3, pressed: true)
-        await robot.rotaryEncoderPress(3, pressed: false)
+        try await robot.rotaryEncoderPress(3, pressed: true)
+        try await robot.rotaryEncoderPress(3, pressed: false)
 
         await robot.assertSnapshot(\.touchAreaImages[0].image, as: .image, named: "dial_right")
         await robot.assertSnapshot(\.touchAreaImages[1].image, as: .image, named: "dial_left")
@@ -62,13 +62,13 @@ final class StreamDeckLayoutTests: XCTestCase {
     }
 
     func test_dial_rotate_and_click_on_touch_area() async throws {
-        await robot.use(.plus, rendering: TestViews.TouchAreaTestLayout())
+        try await robot.use(.plus, rendering: TestViews.TouchAreaTestLayout())
 
-        await robot.rotate(2, steps: 3)
-        await robot.rotate(2, steps: -3)
+        try await robot.rotate(2, steps: 3)
+        try await robot.rotate(2, steps: -3)
 
-        await robot.rotaryEncoderPress(3, pressed: true)
-        await robot.rotaryEncoderPress(3, pressed: false)
+        try await robot.rotaryEncoderPress(3, pressed: true)
+        try await robot.rotaryEncoderPress(3, pressed: false)
 
         await robot.assertSnapshot(\.touchAreaImages[0].image, as: .image, named: "dial_right")
         await robot.assertSnapshot(\.touchAreaImages[1].image, as: .image, named: "dial_left")
@@ -79,12 +79,12 @@ final class StreamDeckLayoutTests: XCTestCase {
     // MARK: Fling
 
     func test_fling_on_touch_area() async throws {
-        await robot.use(.plus, rendering: TestViews.TouchAreaTestLayout())
+        try await robot.use(.plus, rendering: TestViews.TouchAreaTestLayout())
 
-        await robot.fling(startX: 30, startY: 5, endX: 5, endY: 6) // left
-        await robot.fling(startX: 5, startY: 5, endX: 30, endY: 6) // right
-        await robot.fling(startX: 5, startY: 5, endX: 8, endY: 80) // down
-        await robot.fling(startX: 5, startY: 80, endX: 8, endY: 2) // up
+        try await robot.fling(startX: 30, startY: 5, endX: 5, endY: 6) // left
+        try await robot.fling(startX: 5, startY: 5, endX: 30, endY: 6) // right
+        try await robot.fling(startX: 5, startY: 5, endX: 8, endY: 80) // down
+        try await robot.fling(startX: 5, startY: 80, endX: 8, endY: 2) // up
 
         await robot.assertSnapshot(\.touchAreaImages[0].image, as: .image, named: "fling_left")
         await robot.assertSnapshot(\.touchAreaImages[1].image, as: .image, named: "fling_right")
@@ -95,24 +95,24 @@ final class StreamDeckLayoutTests: XCTestCase {
     // MARK: Touch
 
     func test_touch_on_touch_area() async throws {
-        await robot.use(.plus, rendering: TestViews.TouchAreaTestLayout())
+        try await robot.use(.plus, rendering: TestViews.TouchAreaTestLayout())
 
-        await robot.touch(x: 30, y: 20)
-        await robot.touch(x: 80, y: 10)
+        try await robot.touch(x: 30, y: 20)
+        try await robot.touch(x: 80, y: 10)
 
         await robot.assertSnapshot(\.touchAreaImages[0].image, as: .image, named: "30_20")
         await robot.assertSnapshot(\.touchAreaImages[1].image, as: .image, named: "80_10")
     }
 
     func test_touch_on_dial_section() async throws {
-        await robot.use(.plus, rendering: TestViews.SimpleLayout())
+        try await robot.use(.plus, rendering: TestViews.SimpleLayout())
 
         let caps = robot.device.capabilities
 
         for section in 0 ..< caps.dialCount {
             let rect = caps.getTouchAreaSectionDeviceRect(section)
 
-            await robot.touch(x: Int(rect.midX), y: Int(rect.midY))
+            try await robot.touch(x: Int(rect.midX), y: Int(rect.midY))
             await robot.assertSnapshot(\.touchAreaImages[section].image, as: .image, named: "section_\(section)")
         }
     }
